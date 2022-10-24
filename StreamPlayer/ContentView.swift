d831903f30a05ae579d9e7f5218e44280a4d61b5
Knut Lorenzen <lo.knut@gmail.com>
2022-10-24 21:46:45 +0100

    yallah

--- StreamPlayer/ContentView.swift
+++ StreamPlayer/ContentView.swift
@@ -6,16 +6,130 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+struct Stream : Hashable, Codable {
+    let url: URL
+    let name: String
+}
+
+extension Stream : Identifiable {
+    var id: String {
+        return url.absoluteString
+    }
+}
+
+struct AddNewStreamForm : View {
+
+    @Environment(\.presentationMode) var presentationMode
+
+    @State var name = ""
+    @State var url = ""
+    var body: some View {
+
+        VStack {
+            TextField("URL", text: $url)
+            TextField("Name", text: $name)
+            Button("Add") {
+
+                guard !name.isEmpty, let url = URL(string: url) else { return }
+                let streamsData = UserDefaults.standard.data(forKey: "streams") ?? Data()
+                var streamsArray = (try? PropertyListDecoder().decode([Stream].self, from: streamsData)) ?? []
+                streamsArray.append(Stream(url: url, name: name))
+                UserDefaults.standard.setValue(try! PropertyListEncoder().encode(streamsArray), forKey: "streams")
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
+    }
+}
+
 
 struct ContentView: View {
+
+    private enum PlayState {
+        case stopped, paused, playing
+    }
+    @State private var playState: PlayState = .stopped
+    @State private var streams: [Stream] = []
+    private var player = AVPlayer()
+    @State private var sheetIsPresented = false
+    @State private var selection: Stream?
+
+    init() {
+        reloadStreams()
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("Add") {
+                sheetIsPresented = true
+            }
+            .sheet(isPresented: $sheetIsPresented){
+                AddNewStreamForm()
+            }.onDisappear(){
+                reloadStreams()
+            }
+            List(selection: $selection) {
+                ForEach(streams) { stream in
+                    Text(stream.name).onTapGesture {
+                        selection = stream
+                        onStreamSelected(stream: stream)
+                    }
+                }
+            }
+            if let selection = selection, playState != .stopped {
+
+                Text("now playing: \(selection.name)")
+
+                Button(playState == .playing ? "Pause" : "Resume") {
+                    onPlayPauseButtonPressed()
+                }
+            }
+        }
+    }
+
+
+    private func reloadStreams() {
+
+        let streamsData = UserDefaults.standard.data(forKey: "streams") ?? Data()
+        streams = (try? PropertyListDecoder().decode([Stream].self, from: streamsData)) ?? []
+    }
+
+    private func onPlayPauseButtonPressed() {
+
+        switch playState {
+        case .paused:
+            player.play()
+            playState = .playing
+        case .playing:
+            player.pause()
+            playState = .paused
+        default: ()
+        }
+    }
+
+    private func onStreamSelected(stream: Stream) {
+
+        player.replaceCurrentItem(with: AVPlayerItem(url: stream.url))
+        player.play()
+        playState = .playing
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+
+    init() {
+        let streamsArray: [Stream] = []
+        UserDefaults.standard.setValue(try! PropertyListEncoder().encode(streamsArray), forKey: "streams")
+    }
+
     static var previews: some View {
-        ContentView()
+        Group {
+
+            ContentView()
+//            ContentView(streams: [
+//                ]
+//            )
+        }
     }
 }
